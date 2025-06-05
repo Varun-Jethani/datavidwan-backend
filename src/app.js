4d72fa1f6262fdfa8822d008c4edbffd4391b864
Varun <varunjethani@outlook.com>
2025-06-05 14:39:08 +0530

    add extra dev port

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cors from "cors";
 
 const app = express();
 const corsOptions = {
-  origin: ["http://localhost:5173",process.env.FRONTEND_URL, process.env.ADMIN_URL],
+  origin: ["http://localhost:5173","http://localhost:5174",process.env.FRONTEND_URL, process.env.ADMIN_URL],
   optionsSuccessStatus: 200,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
