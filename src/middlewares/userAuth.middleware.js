e6c675273a96c21b7950a1671b8bf40b9878d9f0
SanjanaBhavsar15 <sanjanabhavsar043@gmail.com>
2026-02-11 12:39:31 +0530

    Updated user middleware

--- src/middlewares/userAuth.middleware.js
+++ src/middlewares/userAuth.middleware.js
@@ -3,28 +3,25 @@ import asyncHandler from "../utils/asynchandler.js";
 import jwt from "jsonwebtoken";
 import { ApiError } from "../utils/ApiError.js";
 
-
-
 export const verifyJWT = asyncHandler(async (req, _, next) => {
-    try {
+  const token = req.cookies?.token;
 
-        const token = req.cookies?.token 
-        || req.headers["authorization"]?.replace("Bearer ", "") 
-        || req.headers["Authorization"]?.replace("Bearer ", "")
-        if (!token) {
-            throw new ApiError(401, "Unauthorized request here");
-        }
-    
-        const decodedToken = jwt.verify(token, `${process.env.JWT_SECRET}`)
-        const user = await userModel.findById(decodedToken?.id).select("-password")
-        if (!user) {
-            throw new ApiError(401, "Invalid Access Token");
-        }
-    
-        req.user = user;
-        next();
-    } catch (error) {
-        throw new ApiError(401, error?.message || "Unable to authenticate");
+  if (!token) {
+    throw new ApiError(401, "Not authorized. No token found.");
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await userModel.findById(decoded.id).select("-password");
+
+    if (!user) {
+      throw new ApiError(401, "User not found");
     }
 
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, "Invalid or expired token");
+  }
 });
